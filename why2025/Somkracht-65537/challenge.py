bcf89cffbe6bfb9f5a9a78bce36596a44f44b875
Issa <89082595+pi-1337@users.noreply.github.com>
2025-08-11 01:09:29 +0000

    Update challenge.py

--- why2025/Somkracht-65537/challenge.py
+++ why2025/Somkracht-65537/challenge.py
@@ -2,6 +2,9 @@
 #   Is this how RSA works? I read something about p and q somewhere.
 #   This challenge is provided by Challenge The Cyber.
 
+
+
+# chal code :
 from Crypto.Util.number import *
 
 p = getPrime(1024)
